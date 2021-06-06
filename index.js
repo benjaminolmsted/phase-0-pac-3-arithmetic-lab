85d1ecdc55f4e53889f0654c2ce807bee1d6470c
benjamin <benjamin.olmsted@gmail.com>
2021-06-06 08:29:33 -0700

    passing all

---
+++ index.js
@@ -0,0 +1,33 @@
+function add(num, num2){
+    return num+num2;
+}
+
+function subtract(num, num2){
+    return num-num2;
+}
+
+function multiply(num, num2){
+    return num*num2;
+}
+
+function divide(num, num2){
+    return num/num2;
+}
+
+function increment (n){
+    n++;
+    return n;
+}
+
+function decrement (n){
+    n--;
+    return n;
+}
+
+function makeInt(n){
+   return parseInt(n, 10);
+}
+
+function preserveDecimal(n) {
+    return parseFloat(n);
+}
